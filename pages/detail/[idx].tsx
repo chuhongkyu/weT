b8a7639e4d82374a258f0e15efdf5804ce923d54
Mr.chu <chuhongkyu@gmail.com>
2023-05-05 18:42:01 +0900

    Add Detail

---
+++ pages/detail/[idx].tsx
@@ -0,0 +1,40 @@
+import { ObjectId } from "mongodb";
+import { useRouter } from "next/router";
+import styles from "styles/Detail.module.scss";
+import { connectDB } from "utils/database";
+
+const Detail = ({ data }:any) => {
+    
+    return (
+      <div id={styles.Detail}>
+          <div className={styles.warrper}>
+            <h1>{data.title}</h1>
+            <p className={styles.content}>{data.content}</p>
+          </div>
+      </div>
+    )
+}
+
+export async function getStaticPaths() {
+    const client = await connectDB;
+    const db = client.db('forum');
+    const posts = await db.collection('post').find({}).toArray();
+  
+    const paths = posts.map((post) => ({
+      params: { idx: post._id.toString() },
+    }));
+  
+    return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }:any) {
+    const client = await connectDB;
+    const db = client.db('forum');
+    const data = await db.collection('post').findOne({
+        _id : new ObjectId(params.idx)
+    });
+  
+    return { props: { data: JSON.parse(JSON.stringify(data)) } };
+}
+
+export default Detail;
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,16 +1,25 @@
 import { NextPage } from "next";
+import Link from "next/link";
 import { connectDB } from "utils/database";
+import styles from "styles/Home.module.scss";
 
 const Home: NextPage<{ data: any[] }> = ({ data }) => {
   console.log(data);
   return (
-    <div>
-        <h1>HOME</h1>
-        <ul>
-        {data ? data.map((item) => (
-          <li key={item._id}>{item.content}</li>
-        )): null}
-      </ul>
+    <div id={styles.Home}>
+        <div className={styles.warrper}>
+          <h1>HOME</h1>
+          <ul>
+            {data ? data.map((item) => (
+              <li key={item._id}>
+                <Link href={`/detail/${item._id}`}>
+                  <h5 className={styles.title}>{item.title}</h5>
+                  <p className={styles.content}>{item.content}</p>
+                </Link>
+              </li>
+            )): null}
+          </ul>
+        </div>
     </div>
   )
 }
