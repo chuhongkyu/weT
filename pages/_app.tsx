3edb119ddfb9e5491d3fd8589054acc2279996a5
Mr.chu <chuhongkyu@gmail.com>
2023-05-13 18:52:35 +0900

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>OTT 플랫폼 비교 - 넷플릭스, 왓챠, Tving, Wave</title>
+        <title>We T - OTT 플랫폼 비교 - 넷플릭스, 왓챠, Tving, Wave</title>
         <meta name="description" content="오티티 플랫폼들의 내용을 비교하여 최적의 선택을 도와주는 사이트입니다." />
         <meta name="keywords" content="OTT, 오티티, 넷플릭스, 왓챠, Tving, Wave, OTT 비교, OTT 추천" />
         <meta name="author" content="Your Name" />
