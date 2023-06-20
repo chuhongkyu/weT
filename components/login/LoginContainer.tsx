d986c35e099d3365274d0df39a03a9972c3b5ccc
Mr.chu <chuhongkyu@gmail.com>
2023-06-20 21:50:22 +0900

    Add login

--- components/login/LoginContainer.tsx
+++ components/login/LoginContainer.tsx
@@ -1,4 +1,5 @@
 import { signIn, signOut, useSession } from 'next-auth/react'
+import Link from 'next/link';
 import styles from "styles/Login.module.scss";
 
 export default function LoginContainer() {
@@ -14,9 +15,14 @@ export default function LoginContainer() {
 
   return (
     <div className={styles.container}>
-        <div className={styles.box}>
-            <button onClick={() => signIn('google')}>Sign in with Google</button>
-        </div>
+        <div className={styles.text}>
+            반가워요!<br/>
+          로그인 해주세요.
+      </div>
+      <div className={styles.selects}>
+        {/* <Link className={styles.local} href={"/register/local"}><span>We T 회원가입</span></Link> */}
+        <button className={styles.google} onClick={() => signIn('google')}><span>구글 회원 로그인</span></button>
+      </div>
     </div>
   )
 }
