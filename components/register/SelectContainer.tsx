91bf7209d3800e42e024d66cdc85190357c9cc13
Mr.chu <chuhongkyu@gmail.com>
2023-07-09 20:11:18 +0900

    fixed css

--- components/register/SelectContainer.tsx
+++ components/register/SelectContainer.tsx
@@ -12,7 +12,7 @@ export default function SelectContainer(): JSX.Element {
           계정을 선택해주세요.
       </div>
       <div className={styles.selects}>
-        <Link className={styles.local} href={"/register/local"}><span>We T 회원가입</span></Link>
+        {/* <Link className={styles.local} href={"/register/local"}><span>We T 회원가입</span></Link> */}
         <button className={styles.google} onClick={() => signIn('google')}><span>구글 회원 가입</span></button>
       </div>
 
