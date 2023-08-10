1d0c8b0fc0e20fefe0f1f0df9cf6a50ffc9ceadf
Mr.chu <chuhongkyu@gmail.com>
2023-08-11 01:02:48 +0900

    Add OTT chart style

--- components/contents/Chart.tsx
+++ components/contents/Chart.tsx
@@ -1,8 +1,22 @@
 'use client'
 
 import React, { useEffect, useState } from "react";
-import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Legend } from "recharts";
+import { Pie, PieChart, Cell, Tooltip, Legend } from "recharts";
 import { IData } from "utils/typeGroup";
+
+interface IChart {
+    category: string;
+    number: number;
+}
+
+const COLORS = ['#0088FE', '#00C49F', '#ed1c01', '#ff42d6', '#d53201'];
+
+const style = {
+    top: 250,
+    left: 300,
+    lineHeight: "24px"
+  };
+  
 
 const Chart = (data:any) => {
     const [dummy, setDummy] = useState<IData[]>(data.data)
@@ -54,22 +68,31 @@ const Chart = (data:any) => {
         ])
     },[data])
   return (
-    <RadarChart
-        id={"CHART"}
-        cx={200}
-        cy={200}
-        outerRadius={100}
-        width={400}
-        height={400}
-        data={chart}
-    >
-      <PolarGrid />
-      <PolarAngleAxis dataKey="category" />
-      <PolarRadiusAxis />
-      <Radar name="계시글 수" dataKey="number" stroke="#17a7ce" fill="#17a7ce" fillOpacity={0.6} />
-      {/* <Radar name="Lily" dataKey="B" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6} /> */}
-      <Legend />
-    </RadarChart>
+    <PieChart width={400} height={400}>
+        <Pie
+          data={chart}
+          cx={200}
+          cy={200}
+          innerRadius={40}
+          outerRadius={80}
+          fill="#8884d8"
+          paddingAngle={1}
+          dataKey="number"
+        >
+          {chart.map((entry:IChart, index:number) => (
+            <Cell key={`cell-${entry.category}`} name={entry.category.toUpperCase()} fill={COLORS[index % COLORS.length]}/>
+          ))}
+        </Pie>
+        <Legend
+            iconSize={10}
+            width={150}
+            height={140}
+            layout="vertical"
+            verticalAlign="middle"
+            wrapperStyle={style}
+        />
+        <Tooltip />
+    </PieChart>
   );
 }
 
